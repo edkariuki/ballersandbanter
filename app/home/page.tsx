cc99a994f86f25ba9c4e4f41517c337a4fd5c045
Edwin Kariuki <edwinkariuki@Edwins-MacBook-Air.local>
2026-07-02 21:23:10 +0300

    hero section and navbar

---
+++ app/home/page.tsx
@@ -0,0 +1,10 @@
+import Hero from "../components/hero";
+
+export default function Home() {
+  return (
+    <div>
+      <Hero />
+      {/* Other components or content can go here */}
+    </div>
+  );
+}
