17283df292d4e870c95762277e0a937ca0d399ac
Edwin Kariuki <kariukimugo510@icloud.com>
2026-07-23 23:15:55 +0300

    fix warning

--- app/components/features_section.tsx
+++ app/components/features_section.tsx
@@ -27,6 +27,7 @@ export default function FeatureSection({
           src={image}
           alt={title}
           fill
+          sizes="(max-width: 768px) 100vw, 50vw"
           className="object-cover transition-transform duration-700 group-hover:scale-105"
         />
 
